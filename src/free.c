c34a53051b444900d0f13a7a5d5642a009697b25
Manuel <mhummel@student.42heilbronn.de>
2025-03-11 11:51:26 +0100

    Update free.c
    freeing the loaded texture

--- src/free.c
+++ src/free.c
@@ -6,7 +6,7 @@
 /*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 13:54:17 by mhummel           #+#    #+#             */
-/*   Updated: 2025/03/06 12:35:15 by mhummel          ###   ########.fr       */
+/*   Updated: 2025/03/11 11:29:39 by mhummel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,14 @@ void	free_game(t_game *game)
 		free(game->we_texture);
 	if (game->ea_texture)
 		free(game->ea_texture);
+		if (game->no_tex)
+        mlx_delete_texture(game->no_tex);
+    if (game->so_tex)
+        mlx_delete_texture(game->so_tex);
+    if (game->we_tex)
+        mlx_delete_texture(game->we_tex);
+    if (game->ea_tex)
+        mlx_delete_texture(game->ea_tex);
 	if (game->map)
 	{
 		i = 0;
