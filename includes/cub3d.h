94e0ff1c1d1abc85868317189d279e9c6b793038
Manuel <mhummel@student.42heilbronn.de>
2025-03-11 09:30:19 +0100

    Update cub3d.h
    Added the notes, and all the functions i already made to the header. We now have a working window which can move around, checks the map etc. Only visuals are missing i think

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 09:54:21 by mhummel           #+#    #+#             */
-/*   Updated: 2025/03/05 14:28:41 by mhummel          ###   ########.fr       */
+/*   Updated: 2025/03/11 09:15:51 by mhummel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@
 
 typedef struct s_game
 {
-	char *no_texture;
-	char *so_texture;
-	char *we_texture;
-	char *ea_texture;
+	char *no_texture; // North texture path
+	char *so_texture; // South texture path
+	char *we_texture; // West texture path
+	char *ea_texture; // East texture path
 	int floor_color;
 	int ceiling_color;
 	char **map;
@@ -40,5 +40,39 @@ typedef struct s_game
 	mlx_t *mlx;
 	mlx_image_t *img; // For rendering
 } t_game;
+
+// parsing_elements.c
+void parse_element(t_game *game, char *line);
+int get_rgb(char *line);
+
+// parsing_map.c
+void parse_cub_file(char *filename, t_game *game);
+void parse_map(t_game *game, char *line, int fd);
+void parse_map_lines(t_game *game, char *line, int fd, int *i);
+
+// validate_map.c
+void validate_map(t_game *game);
+int check_enclosure(t_game *game, int i, int j);
+void set_player_position(t_game *game, int i, int j, int *player_count);
+int is_map_line(char *line);
+int is_valid_char(char c);
+
+// utils.c
+void error_exit(char *msg);
+void strip_newline(char *line);
+
+// free.c
+void free_game(t_game *game);
+
+// main.c
+void init_game(t_game *game);
+int check_collision(t_game *game, double new_x, double new_y);
+
+// mlx_utils.c
+void handle_movement(t_game *game, mlx_key_data_t keydata, double move_speed);
+void handle_strafe(t_game *game, mlx_key_data_t keydata, double move_speed);
+void handle_rotation(t_game *game, mlx_key_data_t keydata, double rot_speed);
+void init_mlx(t_game *game);
+void update_player(t_game *game, double move_speed, double rot_speed);
 
 #endif
