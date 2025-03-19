05b43e3afcfcd95c3c99122bd60b39b1571035f3
frocha <frocha@student.42heilbronn.de>
2025-03-19 15:17:42 +0100

    add norminette valid functions

--- src/main.c
+++ src/main.c
@@ -114,47 +114,73 @@ void render(void *param)
     mlx_image_to_window(game->mlx, game->dynamic_layer, 0, 0);
 }
 
-void	add_static_pixels(t_game *game)
+void draw_walls(t_game *game, int base_x_mult, int base_y_mult)
 {
-	int		add_y;
-	int		base_x_mult;
-	int		base_y_mult;
-	int		x;
-    int     i;
+    int x;
+    int y;
 
-	add_y = 1;
-	base_x_mult = CUBE_SIZE;
-	base_y_mult = CUBE_SIZE;
+    y = 0;
+    while (y < CUBE_SIZE)
+    {
+        x = 0;
+        while (x < CUBE_SIZE)
+        {
+            mlx_put_pixel(game->static_layer, base_x_mult + x, base_y_mult + y, 0xFFFFFFFF); // White color
+            x++;
+        }
+        y++;
+    }
+}
+
+void set_player_location(t_game *game, int base_x_mult, int base_y_mult, char cell)
+{
+    if (cell == 'N' || cell == 'S' || cell == 'E' || cell == 'W')
+    {
+        game->pos_x = base_x_mult;
+        game->pos_y = base_y_mult;
+    }
+}
+
+void process_map_row(t_game *game, int i, int *base_x_mult, int *base_y_mult)
+{
+    int x;
+    
+    x = 0;
+    while (game->map[i][x])
+    {
+        if (game->map[i][x] == '1')
+        {
+            draw_walls(game, *base_x_mult, *base_y_mult);
+        }
+        else
+        {
+            set_player_location(game, *base_x_mult, *base_y_mult, game->map[i][x]);
+        }
+        *base_x_mult += CUBE_SIZE;
+        x++;
+    }
+}
+
+void add_static_pixels(t_game *game)
+{
+    int add_y;
+    int base_x_mult;
+    int base_y_mult;
+    int i;
+
+    add_y = 1;
+    base_x_mult = CUBE_SIZE;
+    base_y_mult = CUBE_SIZE;
     i = 0;
-	while (i < game->map_height)
-	{
-		x = 0;
-		while (game->map[i][x])
-		{
-			if (game->map[i][x] == '1')
-				for (int y = 0; y < CUBE_SIZE; y++)
-				{
-					for (int x = 0; x < CUBE_SIZE; x++)
-					{
-						mlx_put_pixel(game->static_layer, base_x_mult + x,
-							base_y_mult + y, 0xFFFFFFFF); // White color
-					}
-				}
-			else if (game->map[i][x] == 'N' || game->map[i][x] == 'S' || game->map[i][x] == 'E'
-				|| game->map[i][x] == 'W')
-			{
-				game->pos_x = base_x_mult;
-				game->pos_y = base_y_mult;
-			}
-			base_x_mult += CUBE_SIZE;
-			x++;
-		}
-		add_y++;
-		base_x_mult = CUBE_SIZE;
-		base_y_mult = CUBE_SIZE * add_y;
-		i++;
-	}
-	mlx_image_to_window(game->mlx, game->static_layer, 0, 0);
+    while (i < game->map_height)
+    {
+        process_map_row(game, i, &base_x_mult, &base_y_mult);
+        add_y++;
+        base_x_mult = CUBE_SIZE;
+        base_y_mult = CUBE_SIZE * add_y;
+        i++;
+    }
+    mlx_image_to_window(game->mlx, game->static_layer, 0, 0);
 }
 
 bool	is_wall(mlx_image_t *image, int x, int y)
