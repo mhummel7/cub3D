bc1d8c0c73c6b9e5c1cb0c41b328d779ecd45b70
frocha <frocha@student.42heilbronn.de>
2025-04-04 21:56:57 +0200

    norminette applied to parsing_map.c

--- src/parsing_map.c
+++ src/parsing_map.c
@@ -12,209 +12,105 @@
 
 #include "../includes/cub3d.h"
 
-void parse_map_lines(t_game *game, char *line, int fd, int *i)
+void	parse_map_lines(t_game *game, char *line, int fd, int *i)
 {
-    char *map_line;
+	char	*map_line;
 
-    strip_newline(line);
-    game->map[*i] = ft_strdup(line);
-    printf("Allocated map[%d]: %p - Content: %s\n", *i, game->map[*i], game->map[*i]);
-    if (!game->map[*i])
-        error_exit("Malloc failed for map line");
-    (*i)++;
-    while ((map_line = get_next_line(fd)) != NULL)
-    {
-        strip_newline(map_line);
-        if (ft_strlen(map_line) == 0)
-        {
-            free(map_line);
-            break ;
-        }
-        game->map[*i] = ft_strdup(map_line);
-        printf("Allocated map[%d]: %p - Content: %s\n", *i, game->map[*i], game->map[*i]);
-        if (!game->map[*i])
-            error_exit("Malloc failed for map line");
-        free(map_line);
-        (*i)++;
-    }
+	strip_newline(line);
+	game->map[*i] = ft_strdup(line);
+	if (!game->map[*i])
+		error_exit("Malloc failed for map line");
+	(*i)++;
+	map_line = get_next_line(fd);
+	while (map_line != NULL)
+	{
+		strip_newline(map_line);
+		if (ft_strlen(map_line) == 0)
+		{
+			free(map_line);
+			break ;
+		}
+		game->map[*i] = ft_strdup(map_line);
+		if (!game->map[*i])
+			error_exit("Malloc failed for map line");
+		free(map_line);
+		(*i)++;
+		map_line = get_next_line(fd);
+	}
 }
 
-static void set_map_width(t_game *game)
+static void	set_map_width(t_game *game)
 {
-    int i;
-    int len;
+	int	i;
+	int	len;
 
-    i = 0;
-    while (i < game->map_height)
-    {
-        len = ft_strlen(game->map[i]);
-        if (len > game->map_width)
-            game->map_width = len;
-        i++;
-    }
-    printf("Map width set to: %d\n", game->map_width);
+	i = 0;
+	while (i < game->map_height)
+	{
+		len = ft_strlen(game->map[i]);
+		if (len > game->map_width)
+			game->map_width = len;
+		i++;
+	}
 }
 
-void parse_map(t_game *game, char *line, int fd)
+void	parse_map(t_game *game, char *line, int fd)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    game->map = malloc(sizeof(char *) * 100);
-    if (!game->map)
-        error_exit("Malloc failed");
-    printf("Allocated game->map: %p\n", game->map);
-    parse_map_lines(game, line, fd, &i);
-    game->map_height = i;
-    game->map[i] = NULL;
-    printf("Map height set to: %d\n", game->map_height);
-    set_map_width(game);
-    printf("Calling validate_map...\n");
-    validate_map(game);
-    printf("validate_map completed\n");
+	i = 0;
+	game->map = malloc(sizeof(char *) * 100);
+	if (!game->map)
+		error_exit("Malloc failed");
+	parse_map_lines(game, line, fd, &i);
+	game->map_height = i;
+	game->map[i] = NULL;
+	set_map_width(game);
+	validate_map(game);
 }
 
-static void handle_line(char *line, t_game *game, int *map_started, int fd)
+static void	handle_line(char *line, t_game *game, int *map_started, int fd)
 {
-    if (ft_strlen(line) == 0)
-    {
-        if (!*map_started)
-            return ;
-        *map_started = 2;
-    }
-    else if (is_map_line(line) && !*map_started)
-    {
-        *map_started = 1;
-        printf("Starting map parsing with line: %s\n", line);
-        parse_map(game, line, fd);
-    }
-    else if (!*map_started)
-        parse_element(game, line);
+	if (ft_strlen(line) == 0)
+	{
+		if (!*map_started)
+			return ;
+		*map_started = 2;
+	}
+	else if (is_map_line(line) && !*map_started)
+	{
+		*map_started = 1;
+		parse_map(game, line, fd);
+	}
+	else if (!*map_started)
+		parse_element(game, line);
 }
 
-void parse_cub_file(char *filename, t_game *game)
+void	parse_cub_file(char *filename, t_game *game)
 {
-    int fd;
-    char *line;
-    int map_started;
+	int		fd;
+	char	*line;
+	int		map_started;
 
-    fd = open(filename, O_RDONLY);
-    map_started = 0;
-    if (fd < 0)
-        error_exit("Cannot open file");
-    while ((line = get_next_line(fd)) != NULL && map_started != 2)
-    {
-        strip_newline(line);
-        printf("Processing line: %s\n", line);
-        handle_line(line, game, &map_started, fd);
-        free(line);
-    }
-    while ((line = get_next_line(fd)) != NULL)
-        free(line);
-    close(fd);
-    if (!game->no_texture || !game->so_texture || !game->we_texture
-        || !game->ea_texture || !game->map)
-        error_exit("Missing required elements");
-    printf("parse_cub_file completed\n");
+	fd = open(filename, O_RDONLY);
+	map_started = 0;
+	if (fd < 0)
+		error_exit("Cannot open file");
+	line = get_next_line(fd);
+	while (line != NULL && map_started != 2)
+	{
+		strip_newline(line);
+		handle_line(line, game, &map_started, fd);
+		free(line);
+		line = get_next_line(fd);
+	}
+	while (line != NULL)
+	{
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	if (!game->no_texture || !game->so_texture || !game->we_texture
+		|| !game->ea_texture || !game->map)
+		error_exit("Missing required elements");
 }
-
-// #include "../includes/cub3d.h"
-
-// void	parse_map_lines(t_game *game, char *line, int fd, int *i)
-// {
-// 	char	*map_line;
-
-// 	strip_newline(line);
-// 	game->map[*i] = ft_strdup(line);
-// 	printf("Allocated map[%d]: %p\n", *i, game->map[*i]);
-// 	if (!game->map[*i])
-// 		error_exit("Malloc failed for map line");
-// 	(*i)++;
-// 	while ((map_line = get_next_line(fd)) != NULL)
-// 	{
-// 		strip_newline(map_line);
-// 		if (ft_strlen(map_line) == 0)
-// 		{
-// 			free(map_line);
-// 			break ;
-// 		}
-// 		game->map[*i] = ft_strdup(map_line);
-// 		printf("Allocated map[%d]: %p\n", *i, game->map[*i]);
-// 		if (!game->map[*i])
-// 			error_exit("Malloc failed for map line");
-// 		free(map_line);
-// 		(*i)++;
-// 	}
-// }
-
-// static void	set_map_width(t_game *game)
-// {
-// 	int	i;
-// 	int	len;
-
-// 	i = 0;
-// 	while (i < game->map_height)
-// 	{
-// 		len = ft_strlen(game->map[i]);
-// 		if (len > game->map_width)
-// 			game->map_width = len;
-// 		i++;
-// 	}
-// }
-
-// void	parse_map(t_game *game, char *line, int fd)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	game->map = malloc(sizeof(char *) * 100);
-// 	if (!game->map)
-// 		error_exit("Malloc failed");
-// 	parse_map_lines(game, line, fd, &i);
-// 	game->map_height = i;
-// 	game->map[i] = NULL;
-// 	game->map_width = 0;
-// 	set_map_width(game);
-// 	validate_map(game);
-// }
-
-// static void	handle_line(char *line, t_game *game, int *map_started, int fd)
-// {
-// 	if (ft_strlen(line) == 0)
-// 	{
-// 		if (!*map_started)
-// 			return ;
-// 		*map_started = 2;
-// 	}
-// 	else if (is_map_line(line) && !*map_started)
-// 	{
-// 		*map_started = 1;
-// 		parse_map(game, line, fd);
-// 	}
-// 	else if (!*map_started)
-// 		parse_element(game, line);
-// }
-
-// void	parse_cub_file(char *filename, t_game *game)
-// {
-// 	int		fd;
-// 	char	*line;
-// 	int		map_started;
-
-// 	fd = open(filename, O_RDONLY);
-// 	map_started = 0;
-// 	if (fd < 0)
-// 		error_exit("Cannot open file");
-// 	while ((line = get_next_line(fd)) != NULL && map_started != 2)
-// 	{
-// 		strip_newline(line);
-// 		handle_line(line, game, &map_started, fd);
-// 		free(line);
-// 	}
-// 	while ((line = get_next_line(fd)) != NULL)
-// 		free(line);
-// 	close(fd);
-// 	if (!game->no_texture || !game->so_texture || !game->we_texture
-// 		|| !game->ea_texture || !game->map)
-// 		error_exit("Missing required elements");
-// }
