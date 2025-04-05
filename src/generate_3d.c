c0ec3bfce2cbf3f48dfb83adc67b1eea51b94a50
frocha <frocha@student.42heilbronn.de>
2025-04-05 10:40:34 +0200

    quick norminette fixes

--- src/generate_3d.c
+++ src/generate_3d.c
@@ -29,10 +29,10 @@ void generate_3d_projection(t_rays *rays, t_player *player)
     int i = 0;
     while(i < NUM_RAYS)
     {
-        float perpDistance = (*rays)[i].distance * cos((*rays)[i].ray_angle - player->rotation_angle);
-        float distanceProjPlane = (SCREEN_WIDTH / 2) / tan(FOV_ANGLE / 2);
-        float projectedWallHeight = (CUBE_SIZE / perpDistance) * distanceProjPlane;
-        int wallStripHeight = (int)projectedWallHeight;
+        float perp_distance = (*rays)[i].distance * cos((*rays)[i].ray_angle - player->rotation_angle);
+        float distance_proj_plane = (SCREEN_WIDTH / 2) / tan(FOV_ANGLE / 2);
+        float projected_wall_height = (CUBE_SIZE / perp_distance) * distance_proj_plane;
+        int wall_strip_height = (int)projected_wall_height;
         float wallX;
         if ((*rays)[i].was_hit_vertical)
             wallX = (*rays)[i].wall_hit_y - floor((*rays)[i].wall_hit_y);
@@ -40,25 +40,25 @@ void generate_3d_projection(t_rays *rays, t_player *player)
             wallX = (*rays)[i].wall_hit_x - floor((*rays)[i].wall_hit_x);
         mlx_texture_t* texture;
         if ((*rays)[i].was_hit_vertical) {
-            texture = ((*rays)[i].ray_angle > M_PI/2 && (*rays)[i].ray_angle < 3*M_PI/2) 
+            texture = ((*rays)[i].ray_angle > PI/2 && (*rays)[i].ray_angle < 3*PI/2) 
                 ? player->game->we_tex : player->game->ea_tex;
         } else {
-            texture = ((*rays)[i].ray_angle > M_PI) 
+            texture = ((*rays)[i].ray_angle > PI) 
                 ? player->game->so_tex : player->game->no_tex;
         }
-        int wallTopPixel = (SCREEN_HEIGHT / 2) - (wallStripHeight / 2);
-        wallTopPixel = wallTopPixel < 0 ? 0 : wallTopPixel;
-        int wallBottomPixel = (SCREEN_HEIGHT / 2) + (wallStripHeight / 2);
-        wallBottomPixel = wallBottomPixel > SCREEN_HEIGHT ? SCREEN_HEIGHT : wallBottomPixel;
-        int y = wallTopPixel;
-        while(y < wallBottomPixel)
+        int wall_top_pixel = (SCREEN_HEIGHT / 2) - (wall_strip_height / 2);
+        wall_top_pixel = wall_top_pixel < 0 ? 0 : wall_top_pixel;
+        int wall_bottom_pixel = (SCREEN_HEIGHT / 2) + (wall_strip_height / 2);
+        wall_bottom_pixel = wall_bottom_pixel > SCREEN_HEIGHT ? SCREEN_HEIGHT : wall_bottom_pixel;
+        int y = wall_top_pixel;
+        while(y < wall_bottom_pixel)
         {
-            float texPosY = (y - wallTopPixel) / (float)wallStripHeight;
-            int texY = (int)(texPosY * texture->height);
-            texY = texY < 0 ? 0 : (texY >= (int)texture->height ? texture->height - 1 : texY);
-            int texX = (int)(wallX * texture->width);
-            texX = texX < 0 ? 0 : (texX >= (int)texture->width ? texture->width - 1 : texX);
-            uint32_t color = get_mlx_texture_color(texture, texX, texY);
+            float tex_pos_y = (y - wall_top_pixel) / (float)wall_strip_height;
+            int tex_y = (int)(tex_pos_y * texture->height);
+            tex_y = tex_y < 0 ? 0 : (tex_y >= (int)texture->height ? texture->height - 1 : tex_y);
+            int tex_x = (int)(wallX * texture->width);
+            tex_x = tex_x < 0 ? 0 : (tex_x >= (int)texture->width ? texture->width - 1 : tex_x);
+            uint32_t color = get_mlx_texture_color(texture, tex_x, tex_y);
             mlx_put_pixel(player->game->wall_layer, i, y, color);
             y++;
         }
