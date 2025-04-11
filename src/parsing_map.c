3ffaff220036dd253d85c6cfa0230e74aed1e0f5
Manuel <mhummel@student.42heilbronn.de>
2025-04-11 11:42:13 +0200

    Closing all open correctly

--- src/parsing_map.c
+++ src/parsing_map.c
@@ -6,7 +6,7 @@
 /*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 13:28:22 by mhummel           #+#    #+#             */
-/*   Updated: 2025/04/07 12:27:19 by mhummel          ###   ########.fr       */
+/*   Updated: 2025/04/11 11:36:07 by mhummel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,10 @@ static void	parse_cub_file_init(char *filename, t_game *game, int *fd,
 		error_exit("Cannot open file");
 	game->filename = ft_strdup(filename);
 	if (!game->filename)
+	{
+		close(*fd);
 		error_exit("Malloc failed for filename");
+	}
 	*map_started = 0;
 }
 
@@ -80,22 +83,39 @@ static void	allocate_map(t_game *game, int map_size)
 		error_exit_game("Malloc failed for map allocation", game);
 }
 
+static int	prepare_map(t_game *game, int fd, int *new_fd)
+{
+	int	map_size;
+
+	if (!game->filename)
+	{
+		close(fd);
+		error_exit_game("Filename not set", game);
+	}
+	map_size = count_map_lines(fd);
+	allocate_map(game, map_size);
+	close(fd);
+	*new_fd = open(game->filename, O_RDONLY);
+	if (*new_fd < 0)
+	{
+		close(*new_fd);
+		error_exit_game("Cannot reopen file", game);
+	}
+	return (map_size);
+}
+
 void	parse_map(t_game *game, char *line, int fd)
 {
 	int	map_size;
 	int	new_fd;
 
-	if (!game->filename)
-		error_exit_game("Filename not set", game);
-	map_size = count_map_lines(fd);
-	allocate_map(game, map_size);
-	close(fd);
-	new_fd = open(game->filename, O_RDONLY);
-	if (new_fd < 0)
-		error_exit_game("Cannot reopen file", game);
+	map_size = prepare_map(game, fd, &new_fd);
 	skip_to_map(new_fd, &line);
 	if (!line)
+	{
+		close(new_fd);
 		error_exit_game("No valid map line found", game);
+	}
 	read_map_lines(game, line, new_fd, map_size);
 	free(line);
 	game->map[map_size] = NULL;
